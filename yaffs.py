2745736aa7b237ce3385b0be1199f9a57425fb40
Matt Boyer <mboyer@sdf.org>
2014-07-03 00:45:49 +0100

    dfsdfs

--- yaffs.py
+++ yaffs.py
@@ -4,41 +4,10 @@ import sys
 import os
 import io
 
-class Block(object):
+class Blob(object):
+
     def __init__(self, bytes):
         self._inner_bytes = bytes
-        self.object_type = self.little_endian_bytes_to_int(0, 32)
-        self.parent_objId = self.little_endian_bytes_to_int(4, 32)
-        self.name_chksum = self.little_endian_bytes_to_int(8, 16)
-        self.name = self.bytes_to_string(10, 100)
-
-    def __str__(self):
-        return str(self._inner_bytes)
-
-    def little_endian_bytes_to_int(self, offset, length):
-        if 0 != length % 8:
-            raise ValueError("length must be a multiple of 8")
-        if length not in (8,16,32):
-            raise ValueError("Weird field length")
-
-        byte_length = int(length / 8)
-        field_bytes = self._inner_bytes[offset:byte_length]
-
-        int_value = int()
-        for byte_idx, byte in enumerate(field_bytes):
-            int_value += byte * (256 ** byte_idx)
-        return int_value
-
-    def bytes_to_string(self, offset, length):
-        # length is in bytes
-        string_value = ''.join([chr(byte) for byte in self._inner_bytes[offset:length]]).strip('\x00')
-        return string_value
-
-class Dumper(object):
-
-    def __init__(self, file_stream):
-        self._stream = file_stream
-        self.spares = list()
 
     @staticmethod
     def bytes_to_binary(bytearray):
@@ -52,32 +21,150 @@ class Dumper(object):
         bit_array = ''.join(bit_array)
         return bit_array
 
+
+    def little_endian_bytes_to_int(self, offset, length):
+        if 0 != length % 8:
+            raise ValueError("length must be a multiple of 8")
+        if length not in (8,16,32):
+            raise ValueError("Weird field length")
+
+        byte_length = int(length / 8)
+        field_bytes = self._inner_bytes[offset:offset + byte_length]
+
+        int_value = int()
+        for byte_idx, byte in enumerate(field_bytes):
+            int_value += byte * (256 ** byte_idx)
+        return int_value
+
+    def bytes_to_string(self, offset, length):
+        # length is in bytes
+        string_value = ''.join([chr(byte) for byte in self._inner_bytes[offset:length]]).strip('\x00')
+        return string_value
+
+class Spare(Blob):
+    def __init__(self, bytes):
+        Blob.__init__(self, bytes)
+
+        assert 64 == len(self._inner_bytes)
+        self._inner_bits = Spare.bytes_to_binary(self._inner_bytes)
+        assert 512 == len(self._inner_bits)
+        self.objectid = self.little_endian_bits_to_int(40, 18)
+
+    def little_endian_bits_to_int(self, bit_offset, length):
+
+        bit_substring = self._inner_bits[bit_offset:bit_offset + length]
+
+        bits_as_int = int()
+        for byte_idx in range(1 + int(length / 8)):
+            range_start = 8 * byte_idx
+            range_end = min(8 * (1 + byte_idx), length)
+            byte_as_int = int(bit_substring[range_start:range_end], 2)
+
+            bits_as_int += byte_as_int * (256 ** byte_idx)
+
+        return bits_as_int
+
+    def __str__(self):
+        return str(self._inner_bits)
+
+    def __repr__(self):
+        return "<spare objectId:{0}>".format(self.objectid)
+
+class ObjectHeader(Blob):
+    header_types = {
+            1: "file",
+            3: "dir",
+            }
+
+    def __init__(self, bytes):
+        Blob.__init__(self, bytes)
+
+        self.object_type = self.little_endian_bytes_to_int(0, 32)
+        self.parent_objid = self.little_endian_bytes_to_int(4, 32)
+        self.name_chksum = self.little_endian_bytes_to_int(8, 16)
+        self.name = self.bytes_to_string(10, 256)
+        self.mode = self.little_endian_bytes_to_int(268, 32)
+        self.uid = self.little_endian_bytes_to_int(272, 32)
+        self.gid = self.little_endian_bytes_to_int(276, 32)
+        self.atime = self.little_endian_bytes_to_int(280, 32)
+        self.mtime = self.little_endian_bytes_to_int(284, 32)
+        self.ctime = self.little_endian_bytes_to_int(288, 32)
+        self.size = self.little_endian_bytes_to_int(292, 32)
+
+    def __str__(self):
+        return str(self._inner_bytes)
+
+    def __repr__(self):
+        return "<{name}:{t} parent {p} size {s} cksum {c}>".format(name=self.name, t=self.header_types[self.object_type], s=self.size, c=self.name_chksum, p=self.parent_objid)
+
+
+class Dumper(object):
+
+    def __init__(self, file_stream):
+        self._stream = file_stream
+        self.spares = list()
+
     @staticmethod
-    def object_id_from_spare(bit_array):
+    def weird_from_spare(bit_array):
         assert 128 == len(bit_array), len(bit_array)
 
-        object_id = bit_array[40:58]
+        object_id = bit_array[20:40]
 
         first_byte_as_int = int(object_id[:8], 2)
         second_byte_as_int = int(object_id[8:16], 2)
-        runt_as_int = int(object_id[16:], 2)
+        runt_as_int = int(object_id[16:20], 2)
 
-        object_id_as_int = 1024 * runt_as_int + 256 * second_byte_as_int + first_byte_as_int
+        weird_as_int = 1024 * runt_as_int + 256 * second_byte_as_int + first_byte_as_int
 
-        return object_id_as_int
+        return weird_as_int
+
+    def read_spare_data(self, block_idx):
+        # We'll read the 2048 bytes of data of index block_idx
+        self._stream.seek(2112 * block_idx, io.SEEK_SET)
+        data_bytes = bytes()
+
+        # Read four times
+        for quartet_idx in range(4):
+            self._stream.seek((1+quartet_idx) * 512, io.SEEK_CUR)
+
+            read_bytes = self._stream.read(16)
+            if 16 != len(read_bytes):
+                raise IOError("Couldn't read spare {0}".format(block_idx))
+            data_bytes += read_bytes
+        assert 64 == len(data_bytes)
+        return Spare(data_bytes)
+
+    def read_all_spares(self):
+        idx = 0
+        while True:
+            try:
+                self.spares.append(self.read_spare_data(idx))
+                idx += 1
+            except IOError:
+                break
+        return idx
 
     def read_block_data(self, block_idx):
-        self._stream.seek(528 * block_idx, io.SEEK_SET)
-        data_bytes = self._stream.read(512)
-        if 512 != len(data_bytes):
-            raise IOError("Couldn't read block {0}".format(block_idx))
-        return Block(data_bytes)
+        # We'll read the 2048 bytes of data of index block_idx
+        self._stream.seek(2112 * block_idx, io.SEEK_SET)
+        data_bytes = bytes()
+
+        # Read four times
+        for quartet_idx in range(4):
+            self._stream.seek(quartet_idx * 16, io.SEEK_CUR)
+            read_bytes = self._stream.read(512)
+            if 512 != len(read_bytes):
+                raise IOError("Couldn't read block {0}".format(block_idx))
+            data_bytes += read_bytes
+        assert 2048 == len(data_bytes)
+        return ObjectHeader(data_bytes)
 
     def find(self, file_name):
 
         matches = list()
-        for idx, spare in enumerate(self.spares):
+        for idx, spare in enumerate(self.spares[::4]):
             block = self.read_block_data(idx)
+            print(block.name)
             if file_name == block.name:
                 matches.append((idx, block, spare))
             else:
@@ -87,20 +174,12 @@ class Dumper(object):
 
     def find_blocks_for_objid(self, objid):
         matches = list()
-        for idx, spare in enumerate(self.spares):
+        for idx, spare in enumerate(self.spares[::4]):
             spare_objid = self.object_id_from_spare(self.bytes_to_binary(spare))
             if objid == spare_objid:
                 matches.append((idx, self.read_block_data(idx)))
         return matches
 
-    def read_spare_data(self):
-        while True:
-            self._stream.seek(512, io.SEEK_CUR)
-            spare_bytes = self._stream.read(16)
-            if len(spare_bytes):
-                self.spares.append(spare_bytes)
-            else:
-                return
 
 def spike():
     # TODO Use argparse instead
@@ -112,35 +191,29 @@ def spike():
 
     with open(file_path, 'rb') as yaffs_file:
         dumper = Dumper(yaffs_file)
-        dumper.read_spare_data()
 
-        print("{0} spares".format(len(dumper.spares)))
-        matches = dumper.find("wpa_supplicant.conf")
-        assert 1 == len(matches)
-        header_idx, header_block, header_spare = matches[0]
+        first_spare = dumper.read_spare_data(0)
+        print(repr(first_spare))
+        print(str(first_spare))
 
-        spare_as_binary = dumper.bytes_to_binary(header_spare)
-        objectid = dumper.object_id_from_spare(spare_as_binary)
-        print("objectId for file is {0}".format(objectid))
+        num_blocks = dumper.read_all_spares()
 
-        matches = dumper.find_blocks_for_objid(objectid)
-        print(len(matches))
-        for (idx, match) in matches:
-            print(str(match))
-            print(dumper.bytes_to_binary(dumper.spares[idx]))
+        print("{0} blocks".format(num_blocks))
 
+        print(repr(dumper.spares[0]))
+        print(str(dumper.spares[0]))
+        return
+        #objectid = 902
+        #matches = dumper.find_blocks_for_objid(objectid)
 
-        # TODO Figure out the length of wpa_supplicant.conf from the header and
-        # cross-ref that to the 16 bytes of spare
+        foo = dumper.read_block_data(3)
+        print(repr(foo))
+        print(str(foo))
 
-        #for idx, spare in enumerate(dumper.spares):
-            #tentative_block = dumper.read_block_data(idx)
-            #if 1 != tentative_block.object_type:
-                #del tentative_block
-                #continue
-            #if 20 > len(tentative_block.name):
-                #print(tentative_block.name)
-
+        idx, first = matches[0]
+        print("Block {0} has matching objid in spare".format(idx))
+        print(repr(first))
+        print(str(first))
 
 if '__main__' == __name__:
     spike()
