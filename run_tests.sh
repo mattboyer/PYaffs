81cd818cc06bc909797e96c72c351b676f9cda65
Matt Boyer <mboyer@sdf.org>
2014-07-12 13:11:54 +0100

    Test stub

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ declare -i PEP_CLEAN NOSE_SUCCESS
 pep8 -v src/*.py
 PEP_CLEAN=${?}
 
-nosetests --with-cov --cover-package=src -v test/
+nosetests --with-cov --cov=src -v test/
 NOSE_SUCCESS=${?}
 
 exit $(( PEP_CLEAN + NOSE_SUCCESS ))
--- test/test_fs_entities.py
+++ test/test_fs_entities.py
@@ -1,5 +1,13 @@
 import unittest
+from mock import Mock
+
+import fs_entities
 
 class Test_FSLeaf(unittest.TestCase):
-    def test_path(self):
+    def setUp(self):
+        self.fs = Mock()
+        self.header = Mock()
+        self.lead = fs_entities.FSLeaf(self.fs, self.header)
+
+    def test_get_bytes(self):
         self.assertEquals(0, 0)
