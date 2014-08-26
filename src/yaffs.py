446f5be01f941f77ada87e4c16b53988ead2de28
Matt Boyer <mboyer@sdf.org>
2014-08-26 19:56:30 +0100

    Lots more to do

--- src/yaffs.py
+++ src/yaffs.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+'''
+PYaffs executable
+'''
+
 import sys
 import os
 import io
@@ -10,6 +14,9 @@ import block_entities
 
 
 class Dumper(object):
+    '''
+    Provides filesystem extraction services.
+    '''
 
     def __init__(self, file_stream):
         self._stream = file_stream
@@ -17,12 +24,16 @@ class Dumper(object):
         self.headers = dict()
 
     def read_spare_data(self, block_idx):
+        '''
+        Returns a Spare object initialised from the spare data present in the
+        block of index `block_idx`
+        '''
         # We'll read the 2048 bytes of data of index block_idx
         self._stream.seek(2112 * block_idx, io.SEEK_SET)
         data_bytes = bytes()
 
         # Read four times
-        for quartet_idx in range(4):
+        for _ in range(4):
             self._stream.seek(512, io.SEEK_CUR)
 
             read_bytes = self._stream.read(16)
@@ -33,6 +44,9 @@ class Dumper(object):
         return block_entities.Spare(data_bytes)
 
     def read_all_spares(self):
+        '''
+        Reads the YAFFS spare metadata for all chunks in the dump.
+        '''
         idx = 0
         while True:
             try:
@@ -43,6 +57,9 @@ class Dumper(object):
         return idx
 
     def read_headers(self):
+        '''
+        Reads the YAFFS object headers present in the dump.
+        '''
         for idx, spare in enumerate(self.spares):
             if not (0 == spare.chunkid and 0xFFFF == spare.bytecount):
                 continue
@@ -50,18 +67,21 @@ class Dumper(object):
             header = block_entities.ObjectHeader(header_bytes, spare.objectid)
             self.headers[header.objectid] = header
 
-    def read_block_data(self, block_idx):
-        num_bytes_to_read = self.spares[block_idx].bytecount
+    def read_block_data(self, chunk_idx):
+        '''
+        Returns the raw data present in the chunk of index `chunk_idx`
+        '''
+        num_bytes_to_read = self.spares[chunk_idx].bytecount
 
-        # We'll read the 2048 bytes of data of index block_idx
-        self._stream.seek(2112 * block_idx, io.SEEK_SET)
+        # We'll read the 2048 bytes of data of index chunk_idx
+        self._stream.seek(2112 * chunk_idx, io.SEEK_SET)
         data_bytes = bytes()
 
         # Read four times
-        for quartet_idx in range(4):
+        for _ in range(4):
             read_bytes = self._stream.read(512)
             if 512 != len(read_bytes):
-                raise IOError("Couldn't read block {0}".format(block_idx))
+                raise IOError("Couldn't read block {0}".format(chunk_idx))
             data_bytes += read_bytes
 
             self._stream.seek(16, io.SEEK_CUR)
@@ -73,6 +93,9 @@ class Dumper(object):
 
 
 def dispatcher():
+    '''
+    Parses CLI arguments and acts accordingly.
+    '''
     parser = argparse.ArgumentParser(description="GRUH")
     parser.add_argument(
         "action",
@@ -93,23 +116,23 @@ def dispatcher():
         print("{0} blocks".format(num_blocks))
         dumper.read_headers()
 
-        fs = fs_entities.FileSystem(dumper)
+        file_system = fs_entities.FileSystem(dumper)
         if args.action in ('ls', 'list'):
             if not args.fs_path:
-                fs.root_object.walk()
+                file_system.root_object.walk()
             else:
-                fs_object = fs.get_obj_from_path(args.fs_path)
+                fs_object = file_system.get_obj_from_path(args.fs_path)
                 if isinstance(fs_object, fs_entities.FSDir):
                     fs_object.walk()
                 else:
                     print(fs_object)
 
         elif 'find' == args.action:
-            results = fs.find(fs_entities.FSFile, 'netdiag')
+            results = file_system.find(fs_entities.FSFile, 'netdiag')
             print(results)
         elif 'extract' == args.action:
             assert args.fs_path
-            file_object = fs.get_obj_from_path(args.fs_path)
+            file_object = file_system.get_obj_from_path(args.fs_path)
             if not isinstance(file_object, fs_entities.FSFile):
                 raise IOError("Cannot extract {0}".format(repr(file_object)))
             if not args.output_path:
